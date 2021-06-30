53699a3ceb6933f07772860d31aa25aca20da1be
Saranga99 <thathsarasaranga12345@gmail.com>
2021-06-29 18:28:25 -0700

    add new demo project

---
+++ demo(spring+angular)/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,12 @@
+package com.example.demo.model;
+
+public class Employee {
+    private long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    private String employeeCode;
+
+}
