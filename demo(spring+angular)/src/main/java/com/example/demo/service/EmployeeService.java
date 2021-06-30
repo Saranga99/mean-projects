a0dbd809a8813d2a621e405770157c1c356bbb89
Saranga99 <thathsarasaranga12345@gmail.com>
2021-06-30 00:26:24 -0700

    update

--- demo(spring+angular)/src/main/java/com/example/demo/service/EmployeeService.java
+++ demo(spring+angular)/src/main/java/com/example/demo/service/EmployeeService.java
@@ -1,4 +1,33 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Employee;
+import com.example.demo.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> finalAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+    
 }
